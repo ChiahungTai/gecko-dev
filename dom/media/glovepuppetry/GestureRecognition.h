b1cddc54e11bc6c5c799726479baa30ca76859be
CTai <glaxy.tai@gmail.com>
2015-08-05 08:01:26 +0800

    Revert "Revert "Initialize Project GlovePuppetry""
    This reverts commit 42289928257b991b81ea50861341624e50b6c3b4.

---
+++ dom/media/glovepuppetry/GestureRecognition.h
@@ -0,0 +1,28 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim:set ts=2 sw=2 sts=2 et cindent: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_dom_GestureRecognition_h
+#define mozilla_dom_GestureRecognition_h
+
+#include "mozilla/Logging.h"
+
+namespace mozilla {
+
+namespace dom {
+
+PRLogModuleInfo* GetGestureRecognitionLog();
+#define GR_LOG(...) MOZ_LOG(GetGestureRecognitionLog(), mozilla::LogLevel::Debug, (__VA_ARGS__))
+
+class GestureRecognition final
+{
+public:
+  explicit GestureRecognition();
+
+};
+
+} // namespace dom
+} // namespace mozilla
+#endif // mozilla_dom_GestureRecognition_h
